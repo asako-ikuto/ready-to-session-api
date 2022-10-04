aacd3fe4b4e95fe592e60b2897dfe777fdae10bd
ikuto-asako <m131932@gmail.com>
2022-10-05 01:49:27 +0900

    midrationファイル修正（名前変更、中身変更）20221005

--- app/database/migrations/2022_10_04_163700_drop_profile_image_from_users_column.php
+++ app/database/migrations/2022_10_04_163700_drop_profile_image_from_users_column.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class DropProfileImageFromUsersTable extends Migration
+class DropProfileImageFromUsersColumn extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class DropProfileImageFromUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::tabel('users', function (Blueprint $table) {
             $table->dropColumn('profile_image');
         });
     }
@@ -25,7 +25,7 @@ class DropProfileImageFromUsersTable extends Migration
      */
     public function down()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::table('users', function (Blueprint $table) {
             $table->string('profile_image')->nullable()->comment('プロフィール画像');
         });
     }
